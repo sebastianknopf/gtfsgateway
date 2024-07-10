738fdeda4809bc595809af25691d4a4bf476bbb5
sebastianknopf <mail@sebastian-knopf.de>
2024-07-10 12:06:57 +0200

    extended extend_feed_info to fill fields contact_email and contact_url

--- gtfsgateway/processing/extend_feed_info.py
+++ gtfsgateway/processing/extend_feed_info.py
@@ -5,13 +5,15 @@ def extend_feed_info(gateway, params):
     cursor = gateway.static_database._connection.cursor()
 
     publisher_name = params['publisher_name']
-    publisher_url = params['publisher_url']         
+    publisher_url = params['publisher_url']
+    contact_email = params['contact_email']
 
     cursor.execute(
-        "UPDATE feed_info SET feed_publisher_name = ?, feed_publisher_url = ?, feed_version = ?",
+        "UPDATE feed_info SET feed_publisher_name = ?, feed_publisher_url = ?, feed_version = ?, feed_contact_email = ?",
         (
             publisher_name,
             publisher_url,
-            datetime.datetime.today().strftime('%Y%m%d%H%M%S')
+            datetime.datetime.today().strftime('%Y%m%d%H%M%S'),
+            contact_email
         )       
     )
