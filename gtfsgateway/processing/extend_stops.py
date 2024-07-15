1eaad2ba1c3994f7580e476a56710c44a82b8277
sebastianknopf <mail@sebastian-knopf.de>
2024-07-15 16:33:33 +0200

    improved extend_stops function to calculate the average position for stations

--- gtfsgateway/processing/extend_stops.py
+++ gtfsgateway/processing/extend_stops.py
@@ -1,3 +1,5 @@
+import logging
+
 def extend_stops(gateway, params):
     cursor = gateway.static_database._connection.cursor()
 
@@ -6,6 +8,7 @@ def extend_stops(gateway, params):
     df_delimiter = params['datafile']['delimiter']
     df_quotechar = params['datafile']['quotechar']
 
+    # run postprocessing using external data file
     df_content = gateway._load_processing_datafile(df_filename, df_columns, df_delimiter, df_quotechar)
     for rd in df_content:
 
@@ -24,3 +27,40 @@ def extend_stops(gateway, params):
                 stop_id
             )
         )
+        
+    # optimize location of parent stations according to their stops
+    cursor.execute("SELECT * FROM stops WHERE location_type = '1'")
+    stations = cursor.fetchall()
+    
+    for station in stations:
+    
+        logging.info(f"updating station {station['stop_id']}")
+    
+        stop_lats = list()
+        stop_lons = list()
+        
+        cursor.execute(
+            "SELECT * FROM stops WHERE (location_type = '' OR location_type = '0') AND parent_station = ?",
+            (
+                station['stop_id'],
+            )
+        )
+        
+        stops = cursor.fetchall()
+ 
+        for stop in stops:
+            stop_lats.append(float(stop['stop_lat']))
+            stop_lons.append(float(stop['stop_lon']))
+            
+        stop_lat = sum(stop_lats) / len(stop_lats)
+        stop_lon = sum(stop_lons) / len(stop_lons)
+        
+        cursor.execute(
+            "UPDATE stops SET stop_lat = ?, stop_lon = ? WHERE location_type = '1' AND stop_id = ?",
+            (
+                stop_lat,
+                stop_lon,
+                station['stop_id']
+            )
+        )
+        
