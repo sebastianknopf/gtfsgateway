59e1f8d585b5064b1a7a98413a12af3721a798b9
sebastianknopf <mail@sebastian-knopf.de>
2024-08-13 09:09:55 +0200

    improved processing function extend_routes in order to use route_short_name instead of route_code

--- gtfsgateway/processing/extend_routes.py
+++ gtfsgateway/processing/extend_routes.py
@@ -23,41 +23,41 @@ def extend_routes(gateway, params):
 
     df_content = gateway._load_processing_datafile(df_filename, df_columns, df_delimiter, df_quotechar)
     for rd in df_content:
+        if rd['route_short_name'] == rd['route_short_name']:
+            route_type = rd['route_type']
+            route_color = rd['route_color']
+            route_color = route_color.lstrip('#')
+     
+            if _calculate_luminance(route_color) >= 140:
+                route_text_color = '000000'
+            else:
+                route_text_color = 'FFFFFF'
+                
+            route_url = rd['route_url']
 
-        route_type = rd['route_type']
-        route_color = rd['route_color']
-        route_color = route_color.lstrip('#')
- 
-        if _calculate_luminance(route_color) >= 140:
-            route_text_color = '000000'
-        else:
-            route_text_color = 'FFFFFF'
-            
-        route_url = rd['route_url']
-
-        if not route_type == '':
-            cursor.execute(
-                "UPDATE routes SET route_type = ?, route_long_name = ?, route_color = ?, route_text_color = ?, route_url = ? WHERE route_short_name = ?",
-                (
-                    route_type,
-                    rd['route_long_name'],
-                    route_color,
-                    route_text_color,
-                    route_url,
-                    rd['route_short_name']
+            if not route_type == '':
+                cursor.execute(
+                    "UPDATE routes SET route_type = ?, route_long_name = ?, route_color = ?, route_text_color = ?, route_url = ? WHERE route_short_name = ?",
+                    (
+                        route_type,
+                        rd['route_long_name'],
+                        route_color,
+                        route_text_color,
+                        route_url,
+                        rd['route_short_name']
+                    )
                 )
-            )
-        else:
-            cursor.execute(
-                "UPDATE routes SET route_long_name = ?, route_color = ?, route_text_color = ?, route_url = ? WHERE route_short_name = ?",
-                (
-                    rd['route_long_name'],
-                    route_color,
-                    route_text_color,
-                    route_url,
-                    rd['route_short_name']
+            else:
+                cursor.execute(
+                    "UPDATE routes SET route_long_name = ?, route_color = ?, route_text_color = ?, route_url = ? WHERE route_short_name = ?",
+                    (
+                        rd['route_long_name'],
+                        route_color,
+                        route_text_color,
+                        route_url,
+                        rd['route_short_name']
+                    )
                 )
-            )
 
 def _calculate_luminance(hexcolor):
     rgb = tuple(int(hexcolor[i:i + 2], 16) for i in (0, 2, 4))
